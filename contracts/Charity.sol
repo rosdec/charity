801eff0023b972540bcff3bd164917735ca81507
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2022-06-21 08:20:56 +0200

    First versions of the smartcontract and the test suite

---
+++ contracts/Charity.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Charity {
+    event CampaignStarted(bytes32 campaignId, address initiator);
+    event WithdrawFunds(bytes32 campaignId, address initiator, uint256 amount);
+    event FundsDonated(bytes32 campaignId, address donor, uint256 amount);
+    event CampaignEnded(bytes32 campaignId, address initiator);
+
+    uint8  private _campaignCount;
+
+    struct Campaign {
+        string title;
+        string imgUrl;
+        string description;
+        uint256 fundsRaised;
+        bool isLive; 
+        address initiator;
+        uint256 deadline;
+        uint256 balance;
+    }
+
+    mapping(uint256 => bytes32) public _campaignsList;
+    mapping(bytes32 => Campaign) public _campaigns;
+    mapping(address => mapping(bytes32 => uint256))
+        public userCampaignDonations;
+
+    constructor() {}
+
+    function getCampaignCount() public view returns (uint8) {
+        return _campaignCount;
+    }
+
+    function generateCampaignId(
+        address initiator,
+        string calldata title,
+        string calldata description
+    ) public pure returns (bytes32) {
+        bytes32 campaignId = keccak256(
+            abi.encodePacked(title, description, initiator)
+        );
+        return campaignId;
+    }
+
+    function startCampaign(
+        string calldata title,
+        string calldata description,
+        string calldata imgUrl,
+        uint256 deadline
+    ) public {
+        // generate a campaignID
+        // using the title, description and the address of the initiator
+        bytes32 campaignId = generateCampaignId(msg.sender, title, description);
+
+        // get a reference to the campaign with the generated Id
+        Campaign storage campaign = _campaigns[campaignId];
+        // require that the campaign is not live yet.
+        require(!campaign.isLive, "Campaign exists");
+        // require the current time to be less than the campaign deadline
+        require(block.timestamp < deadline, "Campaign ended");
+
+        campaign.title = title;
+        campaign.description = description;
+        campaign.initiator = msg.sender;
+        campaign.imgUrl = imgUrl;
+        campaign.deadline = deadline;
+        campaign.isLive = true;
+
+        _campaignsList[_campaignCount] = campaignId;
+
+        // increment the total number of charity campaigns created
+        _campaignCount = _campaignCount + 1;
+
+        // emit an event to the blockchain
+        emit CampaignStarted(campaignId, msg.sender);
+    }
+
+    function endCampaign(bytes32 campaignId) public {
+        Campaign storage campaign = _campaigns[campaignId];
+
+        // require the msg.sender is the creator of the campaign
+        require(msg.sender == campaign.initiator, "Not campaign initiator");
+        // require the campaign is alive
+        require(campaign.isLive, "campaign is not active");
+
+        campaign.isLive = false;
+        campaign.deadline = block.timestamp;
+
+        emit CampaignEnded(campaignId, msg.sender);
+    }
+
+    function getCampaignsInBatch(uint256 _batchNumber) public view returns(bytes32[] memory) {
+        bytes32[] memory campaignsToReturn = new bytes32[](5);
+
+        uint256 index = 5 *_batchNumber;
+
+        for (uint i = 0; i < 5; i++)
+            campaignsToReturn[i] = _campaignsList[index + i];
+
+        return campaignsToReturn;
+    }
+
+    // allows users to donate to a charity campaign of their choice
+    function donateToCampaign(bytes32 campaignId) public payable {
+        // get campaign details with the given campaign
+        Campaign storage campaign = _campaigns[campaignId];
+
+        // end the campaign if the deadline is exceeded
+        if (block.timestamp > campaign.deadline) {
+            campaign.isLive = false;
+        }
+        // require the campaign has not ended
+        require(block.timestamp < campaign.deadline, "Campaign has ended");
+
+        uint256 amountToDonate = msg.value;
+        require(amountToDonate > 0, "Wrong ETH value");
+
+        // increase the campaign balance by the amount donated;
+        campaign.fundsRaised += amountToDonate;
+        campaign.balance += amountToDonate;
+
+        // keep track of users donation history
+        userCampaignDonations[msg.sender][campaignId] = amountToDonate;
+
+        // emit FundsDonated event
+        emit FundsDonated(campaignId, msg.sender, amountToDonate);
+    }
+
+    // returns the details of a campaign given the campaignId
+    function getCampaign(bytes32 campaignId)
+        public
+        view
+        returns (Campaign memory)
+    {
+        return _campaigns[campaignId];
+    }
+
+    function withdrawCampaignFunds(bytes32 campaignId) public {
+        Campaign storage campaign = _campaigns[campaignId];
+
+        // require the msg.sender is the creator of the campaign
+        require(msg.sender == campaign.initiator, "Not campaign initiator");
+        // require the campaign has ended
+        require(!campaign.isLive, "campaign is still active");
+        require(
+            block.timestamp > campaign.deadline,
+            "Campaign is still active"
+        );
+        // require the campaign has funds to be withdrawn
+        require(campaign.balance > 0, "No funds to withdraw");
+
+        uint256 amountToWithdraw = campaign.balance;
+
+        // zero the campaign balance
+        campaign.balance = 0;
+
+        // transfer the balance to the initiator address;
+        payable(campaign.initiator).transfer(amountToWithdraw);
+
+        // emit an event to the blockchain
+        emit WithdrawFunds(campaignId, campaign.initiator, amountToWithdraw);
+    }
+}
---
+++ test/index.ts
@@ -0,0 +1,121 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { string } from "hardhat/internal/core/params/argumentTypes";
+import { Charity } from "../typechain";
+var chai = require('chai');
+
+var contract: Charity;
+var accounts: SignerWithAddress[];
+
+chai.use(require('chai-bignumber')());
+
+before(async function () {
+    const CharityFactory = await ethers.getContractFactory("Charity");
+
+    contract = await CharityFactory.deploy();
+
+    await contract.deployed();
+
+    accounts = await ethers.getSigners();
+})
+
+describe("Charity contract", function () {
+    it("No campagin defined after deployment", async function () {
+        expect(await contract.getCampaignCount()).to.equal(0);
+    });
+
+    it("A new campaign is created", async () => {
+        const tx = await contract.startCampaign(
+            "Test campaign",
+            "This is the description",
+            "http://localhost:3000/conference-64.png",
+            Math.round(new Date().getTime() / 1000) + 3600);
+        await tx.wait();
+
+        expect(await contract.getCampaignCount()).to.equal(1);
+    })
+
+    it("A new campaign is correctly created", async () => {
+        const campaginId = await contract.generateCampaignId(
+            accounts[0].address,
+            "Test campaign",
+            "This is the description");
+        const campaign = await contract.getCampaign(campaginId);
+
+        expect(campaginId).not.equal(0);
+        expect(campaign.title).be.equal("Test campaign");
+        expect(campaign.description).be.equal("This is the description");
+    })
+
+    it("Creates 5 more campaigns", async () => {
+        for (let i = 0; i < 5; i++) {
+            const tx = await contract.startCampaign(
+                "Test campaign number " + String(i),
+                "This is the description for campagin number " + String(i),
+                "http://localhost:3000/conference-64.png",
+                Math.round(new Date().getTime() / 1000) + 3600);
+            await tx.wait();
+        }
+        expect(await contract.getCampaignCount()).to.equal(6);
+    })
+
+    it("Correctly returns a batch of the first 5 campaigns", async () => {
+        const campaigns: any[] = await contract.getCampaignsInBatch(0);
+
+        let campaign = await contract.getCampaign(campaigns[0]);
+
+        expect(campaign.title).equal("Test campaign")
+        expect(campaigns.length).equals(5);
+    })
+
+    it("Campaign 1 receive a donation", async () => {
+        const campaignId = await contract.generateCampaignId(
+            accounts[0].address,
+            "Test campaign",
+            "This is the description");
+
+        await contract.connect(accounts[1]).donateToCampaign(campaignId, { value: ethers.utils.parseEther('0.5') });
+
+        const campaign = await contract.getCampaign(campaignId);
+
+        expect(campaign.balance).above(0);
+    })
+
+    it("Campaign 1 is closed by its initiator", async () => {
+        const campaignId = await contract.generateCampaignId(
+            accounts[0].address,
+            "Test campaign",
+            "This is the description");
+
+        await contract.connect(accounts[0]).endCampaign(campaignId);
+
+        const campaign = await contract.getCampaign(campaignId);
+
+        expect(campaign.isLive).is.false;
+    })
+
+    it("The initiator for Campaign 1 withdraws its money", async () => {
+        const campaignId = await contract.generateCampaignId(
+            accounts[0].address,
+            "Test campaign",
+            "This is the description");
+
+        let campaign = await contract.getCampaign(campaignId);
+
+        const accountPreviousBalance = await accounts[0].getBalance();
+
+        await contract.connect(accounts[0]).withdrawCampaignFunds(campaignId, { gasLimit: 30000000 });
+
+        campaign = await contract.getCampaign(campaignId);
+
+        console.log(accountPreviousBalance);
+        console.log(await accounts[0].getBalance());
+
+        // Campaign balance is 0
+        expect(campaign.balance).equals(0);
+
+        // Campaign initiator balance is increased
+        expect(await accounts[0].getBalance()).is.above(accountPreviousBalance);
+    })
+});
